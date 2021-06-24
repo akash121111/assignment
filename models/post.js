4f054182d970317f5f3e63480422515033cc8d7a
akash121111 <akashkumar121111@gmail.com>
2021-06-24 22:35:42 +0530

    post models and comment models created

---
+++ models/post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: [ true, 'please provide post title' ]
+		},
+
+		description: {
+			type: String
+		},
+
+		like: [ { type: mongoose.Schema.Types.ObjectId, ref: 'users' } ],
+		dislike: [ { type: mongoose.Schema.Types.ObjectId, ref: 'users' } ],
+
+		comments: [ { type: mongoose.Schema.Types.ObjectId, ref: 'comments' } ]
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
